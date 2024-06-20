d014b44779998c18a68f69d1050b4f9f17b5c719
Robert Jank <robert.jank@yahoo.com>
2024-06-20 15:38:17 -0500

    Final Push

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Comment from './Comment';
+import React from "react";
+import Comment from "./Comment";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
-    <div className='comments-wrapper'>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+    <div className="comments-wrapper">
+      {comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
     </div>
   );
 };
